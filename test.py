519c49e4e718d22c6908d6d523d04fa34a61f17a
Cansero <131320384+Cansero@users.noreply.github.com>
2023-07-05 00:09:32 -0400

    Test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import gspread
+import numpy as np
 
 gc = gspread.oauth(
     credentials_filename='credentials.json',
@@ -8,7 +9,7 @@ gc = gspread.oauth(
 
 cm_package = gc.open('CM NSHIP PACKAGE INFORMATION 2023')
 main_file = pd.DataFrame(cm_package.worksheet('MAIN FILE').get_all_records())
-
+main_file_inbound = main_file['Inbound tracking'].to_list()
 
 unreceived = pd.read_csv('unreceived.csv')
 
@@ -20,14 +21,25 @@ mascara = unreceived['Inbound tracking'].isin(['link to amazon', 'Tracking Avail
 
 link = unreceived.loc[~mascara]
 
-print(link.shape)
+link_inbound = link['Inbound tracking'].to_list()
 
-mascara1 = link['Inbound tracking'].isin(main_file['Inbound tracking'])
-
-compare = link.loc[~mascara1]
-
-print(compare.shape)
-
-print(main_file.shape)
+no_match = []
+match = []
+for i in main_file_inbound:
+    if type(i) is not str:
+        i = str(i)
+    for i2 in link_inbound:
+        if len(i) == len(i2):
+            turn = 0
+            different = False
+            while turn < len(i):
+                if i[turn] != i2[turn]:
+                    different = True
+                    break
+                else:
+                    turn += 1
+            if not different:
+                if i not in match:
+                    match.append(i)
 
 print('fin')
